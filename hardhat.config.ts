14387de4578f990b58142fb0103919dbd1bd9f58
daiki saito <daiki.saito92@gmail.com>
2024-01-09 00:33:23 -0800

    feat: init hardhat

---
+++ hardhat.config.ts
@@ -0,0 +1,9 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-foundry";
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.19",
+};
+
+export default config;
