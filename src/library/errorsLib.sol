4a1f45f6d1cd1b27b4e89764ee6a5c5929d43c1c
joe dakwa <joedakwa@hotmail.co.uk>
2024-02-07 10:01:40 +0000

    updated errors lib

--- src/library/errorsLib.sol
+++ src/library/errorsLib.sol
@@ -9,5 +9,12 @@ library errorsLib {
         string internal constant INVALID_PAIR_TOKEN = "Invalid pair token";
         string internal constant EMPTY_SOURCE_TARGET_ROUTER = "Empty source/target router";
         string internal constant NOT_ENOUGH_RECEIVED_AMOUNT = "Not enough received amount";
+        // string internal constant UNISWAP_V2_LIBRARY_IDENTICAL_ADDRESSES = "UniswapV2Library: IDENTICAL_ADDRESSES";
+        // string internal constant UNISWAP_V2_LIBRARY_ZERO_ADDRESS = "UniswapV2Library: ZERO_ADDRESS";
+        // string internal constant UNISWAP_V2_LIBRARY_INSUFFICIENT_AMOUNT = "UniswapV2Library: INSUFFICIENT_AMOUNT";
+        // string internal constant UNISWAP_V2_LIBRARY_INSUFFICIENT_LIQUIDITY = "UniswapV2Library: INSUFFICIENT_LIQUIDITY";
+        // string internal constant UNISWAP_V2_LIBRARY_INSUFFICIENT_INPUT_AMOUNT = "UniswapV2Library: INSUFFICIENT_INPUT_AMOUNT";
+        // string internal constant UNISWAP_V2_LIBRARY_INSUFFICIENT_OUTPUT_AMOUNT = "UniswapV2Library: INSUFFICIENT_OUTPUT_AMOUNT";
+        // string internal constant UNISWAP_V2_LIBRARY_INVALID_PATH = "UniswapV2Library: INVALID_PATH";
 
 }
