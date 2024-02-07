9d8c777ffe9726d6862f3cecc93b82752f385196
joe dakwa <joedakwa@hotmail.co.uk>
2024-02-07 10:18:08 +0000

    refined errors lib

--- src/library/errorsLib.sol
+++ src/library/errorsLib.sol
@@ -10,6 +10,7 @@ library errorsLib {
         string internal constant OUT_OF_BLOCK = "Out of block";
         string internal constant NO_PROFIT = "No profit";
         string internal constant INVALID_PAIR_ADDRESS = "Invalid pair address";
+        string internal constant INVALID_SENDER = "Invalid sender";
         string internal constant INVALID_PAIR_TOKEN = "Invalid pair token";
         string internal constant EMPTY_SOURCE_TARGET_ROUTER = "Empty source/target router";
         string internal constant NOT_ENOUGH_RECEIVED_AMOUNT = "Not enough received amount";
