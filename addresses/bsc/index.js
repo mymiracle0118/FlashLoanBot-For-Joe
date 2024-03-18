7725472eed86be4497fed1e3eeb08915fdf53010
mymircle0118 <noahsflood908@gmail.com>
2024-03-18 19:12:28 +0900

    modify web3 part

--- addresses/bsc/index.js
+++ addresses/bsc/index.js
@@ -7,6 +7,12 @@ const apeMainnet = require('./ape-mainnet.json');
 
 const BNB_MAINNET = '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c';
 const BUSD_MAINNET = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56';
+const TFT_MAINNET = '0x8f0FB159380176D324542b3a7933F0C2Fd0c2bbf';
+const BTCB_MAINNET = '0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c';
+
+const createPair = () => {
+
+}
 
 module.exports = {
   mainnet: {
@@ -69,6 +75,133 @@ module.exports = {
       amountTokenPay: process.env.BNB_AMOUNT,
       tokenPay: BNB_MAINNET,
       tokenSwap: BUSD_MAINNET,
+      sourceRouter: bakeryMainnet.router,
+      targetRouter: pancakeMainnet.router,
+      sourceFactory: bakeryMainnet.factory,
+    },
+    {
+      name: 'BNB to TFT, pancake>panther',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: TFT_MAINNET,
+      sourceRouter: pancakeMainnet.router,
+      targetRouter: pantherMainnet.router,
+      sourceFactory: pancakeMainnet.factory,
+    },
+    {
+      name: 'BNB to TFT, panther>pancake',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: TFT_MAINNET,
+      sourceRouter: pantherMainnet.router,
+      targetRouter: pancakeMainnet.router,
+      sourceFactory: pantherMainnet.factory,
+    },
+    {
+      name: 'BNB to TFT, pancake>ape',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: TFT_MAINNET,
+      sourceRouter: pancakeMainnet.router,
+      targetRouter: apeMainnet.router,
+      sourceFactory: pancakeMainnet.factory,
+    },
+    {
+      name: 'BNB to TFT, ape>pancake',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: TFT_MAINNET,
+      sourceRouter: apeMainnet.router,
+      targetRouter: pancakeMainnet.router,
+      sourceFactory: apeMainnet.factory,
+    },
+    {
+      name: 'BNB to TFT, pancake>bakery',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: TFT_MAINNET,
+      sourceRouter: pancakeMainnet.router,
+      targetRouter: bakeryMainnet.router,
+      sourceFactory: pancakeMainnet.factory,
+    },
+    {
+      name: 'BNB to TFT, bakery>pancake',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: TFT_MAINNET,
+      sourceRouter: bakeryMainnet.router,
+      targetRouter: pancakeMainnet.router,
+      sourceFactory: bakeryMainnet.factory,
+    },
+    {
+      name: 'BNB to TFT, bakery>pancake',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: TFT_MAINNET,
+      sourceRouter: bakeryMainnet.router,
+      targetRouter: pancakeMainnet.router,
+      sourceFactory: bakeryMainnet.factory,
+    },
+    
+    {
+      name: 'BNB to BTCB, pancake>panther',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: BTCB_MAINNET,
+      sourceRouter: pancakeMainnet.router,
+      targetRouter: pantherMainnet.router,
+      sourceFactory: pancakeMainnet.factory,
+    },
+    {
+      name: 'BNB to BTCB, panther>pancake',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: BTCB_MAINNET,
+      sourceRouter: pantherMainnet.router,
+      targetRouter: pancakeMainnet.router,
+      sourceFactory: pantherMainnet.factory,
+    },
+    {
+      name: 'BNB to BTCB, pancake>ape',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: BTCB_MAINNET,
+      sourceRouter: pancakeMainnet.router,
+      targetRouter: apeMainnet.router,
+      sourceFactory: pancakeMainnet.factory,
+    },
+    {
+      name: 'BNB to BTCB, ape>pancake',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: BTCB_MAINNET,
+      sourceRouter: apeMainnet.router,
+      targetRouter: pancakeMainnet.router,
+      sourceFactory: apeMainnet.factory,
+    },
+    {
+      name: 'BNB to BTCB, pancake>bakery',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: BTCB_MAINNET,
+      sourceRouter: pancakeMainnet.router,
+      targetRouter: bakeryMainnet.router,
+      sourceFactory: pancakeMainnet.factory,
+    },
+    {
+      name: 'BNB to BTCB, bakery>pancake',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: BTCB_MAINNET,
+      sourceRouter: bakeryMainnet.router,
+      targetRouter: pancakeMainnet.router,
+      sourceFactory: bakeryMainnet.factory,
+    },
+    {
+      name: 'BNB to BTCB, bakery>pancake',
+      amountTokenPay: process.env.BNB_AMOUNT,
+      tokenPay: BNB_MAINNET,
+      tokenSwap: BTCB_MAINNET,
       sourceRouter: bakeryMainnet.router,
       targetRouter: pancakeMainnet.router,
       sourceFactory: bakeryMainnet.factory,
